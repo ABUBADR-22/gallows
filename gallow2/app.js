62b73bb7172608b701fe97b2e294e1d27e84b7c1
fred <ugodayconzass1995@gmail.com>
2020-11-26 11:37:13 +0300

    26 11:37

--- gallow2/app.js
+++ gallow2/app.js
@@ -4,13 +4,18 @@ let words = ["ГОНДЖУБАС", "СИГАРА", "ШПАК"],
    btnNx = document.querySelector('.next');
 
 btnSt.addEventListener("click", function fred() {
-   let word = words[Math.floor(Math.random() * words.length)],
-      div = document.querySelector('.fer'),
+   let div = document.querySelector('.fer'),
       key_buttons = document.querySelectorAll(".key"),
-      y = 0;
+      y = 0,
       board = document.querySelector('.gallows__keyboard'),
       arr2 = [],
       arr3 = [];
+
+   btnSt.classList.toggle('startbtn');
+   board.classList.toggle('start');
+   div.classList.toggle('start');
+   btnNx.classList.toggle('start');
+   div.innerHTML = '';
 
    for (let i = 0; i < words.length; i++) {
       word2 = Math.floor(Math.random() * words.length);
@@ -21,57 +26,56 @@ btnSt.addEventListener("click", function fred() {
          i--;
       }
    }
- key_buttons.forEach((e) => {
-      e.classList.remove('vibration', "delete");
-   });
 
    let arrWord = arr3[0].split('');
-   function fred(e) {
-      arrWord.forEach(function (item, i, arr) {
+
+   function fred(el) {
+      key_buttons.forEach((e) => {
+         e.classList.remove('vibration', "delete");
+      });
+
+      el.forEach(function (item, i, arr) {
          div.insertAdjacentHTML("afterBegin", `<div class="
          letter-cont none"><div class="letter">`);
          let letter = document.querySelector('.letter');
          letter.insertAdjacentHTML("afterBegin", arr[arr.length - i - 1]);
          div.insertAdjacentHTML("afterBegin", `</div></div>`);
       });
-      console.log(arrWord)
+
+      let letters = document.querySelectorAll('.letter');
+
+      key_buttons.forEach((element) => {
+         element.addEventListener("click", function (e) {
+            
+            console.log(e.target.textContent);
+
+            if (el.includes(e.target.textContent)) {
+               e.target.classList.add('delete');
+               for (let i in el) {
+                  if (el[i] === e.target.textContent) {
+                     letters[i].style.opacity = "1";
+                  }
+               }
+            } else {
+               e.target.classList.add('vibration');
+            }
+         });
+      });
    }
-   
    fred(arrWord);
 
-   // btnNx.addEventListener("click", function fred() {
-   //    if (y < words.length){
-   //       y++;
-   //       arrWord = arr3[y].split('');
-   //       console.log(arrWord);
-   //       fred(arrWord);
-         
-   //    }
-   //    else{
-   //       y = -1;
-   //    }
-   // });
-
-   btnSt.classList.toggle('startbtn');
-   board.classList.toggle('start');
-   div.classList.toggle('start');
-   div.innerHTML = '';
-
-   let letters = document.querySelectorAll('.letter');
-   console.log(letters)
-
-   key_buttons.forEach((element) => {
-      element.addEventListener("click", function (e) {
-         if (arrWord.includes(e.target.textContent)) {
-            e.target.classList.add('delete');
-            for (let i in arrWord) {
-               if (arrWord[i] === e.target.textContent) {
-                  letters[i].style.opacity = "1";
-               }
-            }
-         } else {
-            e.target.classList.add('vibration');
-         }
-      });
+   btnNx.addEventListener("click", function fredded() {
+      if (y < words.length - 1) {
+         y++;
+         div.innerHTML = '';
+         arrWord = arr3[y].split('');
+         console.log(arrWord);
+         fred(arrWord);
+      }
+      else {
+         y = -1;
+      }
    });
+
+
 })
